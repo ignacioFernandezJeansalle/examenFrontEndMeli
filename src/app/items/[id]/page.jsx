61c8a8c3d738b16137fae8c7b227643550bf6eb1
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-01-26 16:56:55 -0300

    Arregle bug con el metadata dinamico

--- src/app/items/[id]/page.jsx
+++ src/app/items/[id]/page.jsx
@@ -1,19 +1,20 @@
+import { resolve } from "styled-jsx/css";
 import ItemDetailContainer from "../../components/ItemDetailContainer/ItemDetailContainer";
 
 export async function generateMetadata({ params }) {
   const { id } = params;
 
-  const data = await fetch(`http://localhost:3001/api/items/${id}`).then((response) => {
-    if (response.status === 200) {
-      response.json();
-    } else {
-      return { item: { title: "", description: "" } };
-    }
-  });
+  let data = {};
+  const response = await fetch(`http://localhost:3001/api/items/${id}`);
+  if (response.ok) {
+    data = await response.json();
+  } else {
+    data = { item: { title: "", description: "" } };
+  }
 
   return {
-    title: `${data?.item.title} | MercadoLibre 📦`,
-    description: `✓ Comprá online de manera segura con Compra Protegida © ${data?.item.title} ❤`,
+    title: `${data.item.title} | MercadoLibre 📦`,
+    description: `✓ Comprá online de manera segura con Compra Protegida © ${data.item.title} ❤`,
   };
 }
 
